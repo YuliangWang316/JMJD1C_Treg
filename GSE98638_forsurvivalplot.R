a98549fd7ba76cbe1cca1f85cd73f53e64e158db
YuliangWang316 <60790093+YuliangWang316@users.noreply.github.com>
2023-02-15 23:26:54 +0800

    Add files via upload

---
+++ GSE98638_forsurvivalplot.R
@@ -0,0 +1,197 @@
+library(dplyr)
+library(Seurat)
+
+pbmc.data<-read.table("c:/Users/xjmik/Downloads/GSE98638/GSE98638_HCC.TCell.S5063.count.txt",sep = "\t",header = TRUE,row.names = 1,check.names = FALSE)
+pbmc.data<-pbmc.data[!duplicated(pbmc.data$symbol),]
+pbmc.data<-na.omit(pbmc.data)
+rownames(pbmc.data)<-pbmc.data$symbol
+pbmc.data<-pbmc.data[,-1]
+pbmc.metadata<-read.table("c:/Users/xjmik/Downloads/GSE98638/metadata.txt",sep = "\t",header = TRUE,row.names = 1)
+pbmc.metadata<-pbmc.metadata[colnames(pbmc.data),]
+pbmc <- CreateSeuratObject(counts = pbmc.data, project = "pbmc3k",meta.data = pbmc.metadata)
+remove(pbmc.data,pbmc.metadata)
+pbmc <- NormalizeData(pbmc)
+pbmc <- FindVariableFeatures(pbmc, selection.method = "vst", nfeatures = 2000)
+all.genes <- rownames(pbmc)
+pbmc <- ScaleData(pbmc, features = all.genes)
+remove(all.genes)
+pbmc <- RunPCA(pbmc, features = VariableFeatures(object = pbmc))
+pbmc <- FindNeighbors(pbmc, dims = 1:20)
+pbmc <- FindClusters(pbmc, resolution = 0.8)
+pbmc <- RunUMAP(pbmc,dims = 1:20)
+Idents(pbmc)<-pbmc@meta.data$majorCluster
+
+
+for (i in 1:length(rownames(pbmc@meta.data))) {
+  if(pbmc@meta.data$sampleType[i] == "PTH"){
+    pbmc@meta.data$Bulksample[i] <- "P"
+  }
+  if(pbmc@meta.data$sampleType[i] == "PTC"){
+    pbmc@meta.data$Bulksample[i] <- "P"
+  }
+  if(pbmc@meta.data$sampleType[i] == "PTR"){
+    pbmc@meta.data$Bulksample[i] <- "P"
+  }
+  if(pbmc@meta.data$sampleType[i] == "TTH"){
+    pbmc@meta.data$Bulksample[i] <- "T"
+  }
+  if(pbmc@meta.data$sampleType[i] == "TTR"){
+    pbmc@meta.data$Bulksample[i] <- "T"
+  }
+  if(pbmc@meta.data$sampleType[i] == "TTC"){
+    pbmc@meta.data$Bulksample[i] <- "T"
+  }
+  if(pbmc@meta.data$sampleType[i] == "JTH"){
+    pbmc@meta.data$Bulksample[i] <- "J"
+  }
+  if(pbmc@meta.data$sampleType[i] == "JTR"){
+    pbmc@meta.data$Bulksample[i] <- "J"
+  }
+  if(pbmc@meta.data$sampleType[i] == "JTC"){
+    pbmc@meta.data$Bulksample[i] <- "J"
+  }
+  if(pbmc@meta.data$sampleType[i] == "NTH"){
+    pbmc@meta.data$Bulksample[i] <- "N"
+  }
+  if(pbmc@meta.data$sampleType[i] == "NTR"){
+    pbmc@meta.data$Bulksample[i] <- "N"
+  }
+  if(pbmc@meta.data$sampleType[i] == "NTC"){
+    pbmc@meta.data$Bulksample[i] <- "N"
+  }
+}
+remove(i)
+
+for (i in 1:length(rownames(pbmc@meta.data))) {
+  if(pbmc@meta.data$sampleType[i] == "PTH"){
+    pbmc@meta.data$Bulksample2[i] <- "TH"
+  }
+  if(pbmc@meta.data$sampleType[i] == "PTC"){
+    pbmc@meta.data$Bulksample2[i] <- "TC"
+  }
+  if(pbmc@meta.data$sampleType[i] == "PTR"){
+    pbmc@meta.data$Bulksample2[i] <- "TR"
+  }
+  if(pbmc@meta.data$sampleType[i] == "TTH"){
+    pbmc@meta.data$Bulksample2[i] <- "TH"
+  }
+  if(pbmc@meta.data$sampleType[i] == "TTR"){
+    pbmc@meta.data$Bulksample2[i] <- "TR"
+  }
+  if(pbmc@meta.data$sampleType[i] == "TTC"){
+    pbmc@meta.data$Bulksample2[i] <- "TC"
+  }
+  if(pbmc@meta.data$sampleType[i] == "JTH"){
+    pbmc@meta.data$Bulksample2[i] <- "TH"
+  }
+  if(pbmc@meta.data$sampleType[i] == "JTR"){
+    pbmc@meta.data$Bulksample2[i] <- "TR"
+  }
+  if(pbmc@meta.data$sampleType[i] == "JTC"){
+    pbmc@meta.data$Bulksample2[i] <- "TC"
+  }
+  if(pbmc@meta.data$sampleType[i] == "NTH"){
+    pbmc@meta.data$Bulksample2[i] <- "TH"
+  }
+  if(pbmc@meta.data$sampleType[i] == "NTR"){
+    pbmc@meta.data$Bulksample2[i] <- "TR"
+  }
+  if(pbmc@meta.data$sampleType[i] == "NTC"){
+    pbmc@meta.data$Bulksample2[i] <- "TC"
+  }
+}
+remove(i)
+Idents(pbmc)<-pbmc@meta.data$Bulksample2
+
+Idents(pbmc)<-pbmc$majorCluster
+Treg<-subset(pbmc,idents = c("C08_CD4-CTLA4"))
+Treg <- NormalizeData(Treg)
+Treg <- FindVariableFeatures(Treg, selection.method = "vst", nfeatures = 2000)
+all.genes <- rownames(Treg)
+Treg <- ScaleData(Treg, features = all.genes)
+Treg <- RunPCA(Treg, features = VariableFeatures(object = Treg))
+
+Treg <- FindNeighbors(Treg, dims = 1:10)
+Treg <- FindClusters(Treg, resolution = 0.4)
+Treg <- RunUMAP(Treg, dims = 1:10)
+Treg <- RunTSNE(Treg, dims = 1:10)
+
+
+library(ggpubr)
+library(RColorBrewer)
+remove(all.genes)
+new.cluster.ids <- c("lo","lo","hi","hi","lo")
+names(new.cluster.ids) <- levels(Treg)
+Treg <- RenameIdents(Treg, new.cluster.ids)
+
+remove(new.cluster.ids)
+
+Tregtumor1chi<-subset(Treg,idents = "hi")
+Tregtumor1clo<-subset(Treg,idents = "lo")
+remove(Treg)
+Tregpbmc<-subset(pbmc,idents = "C07_CD4-FOXP3")
+
+JMJD1Chi.data <- as.data.frame(Tregtumor1chi@assays$RNA@counts)
+JMJD1Clo.data <- as.data.frame(Tregtumor1clo@assays$RNA@counts)
+Tregpbmc.data <- as.data.frame(Tregpbmc@assays$RNA@counts)
+remove(Tregtumor1chi,Tregtumor1clo,Tregpbmc)
+
+
+JMJD1Chi.metadata<-data.frame(colnames(JMJD1Chi.data),rep("hi",length(colnames(JMJD1Chi.data))))
+JMJD1Clo.metadata<-data.frame(colnames(JMJD1Clo.data),rep("lo",length(colnames(JMJD1Clo.data))))
+Tregpbmc.metadata<-data.frame(colnames(Tregpbmc.data),rep("pbmc",length(colnames(Tregpbmc.data))))
+colnames(JMJD1Chi.metadata)<-c("barcode","group")
+colnames(JMJD1Clo.metadata)<-c("barcode","group")
+colnames(Tregpbmc.metadata)<-c("barcode","group")
+pbmc.metadata<-rbind(JMJD1Chi.metadata,JMJD1Clo.metadata,Tregpbmc.metadata)
+rownames(pbmc.metadata)<-pbmc.metadata[,1]
+pbmc.data<-cbind(JMJD1Chi.data,JMJD1Clo.data,Tregpbmc.data)
+remove(i,JMJD1Chi.metadata,JMJD1Clo.metadata,Tregpbmc.metadata,JMJD1Chi.data,JMJD1Clo.data,Tregpbmc.data)
+pbmc_raw<-pbmc
+remove(pbmc)
+pbmc <- CreateSeuratObject(counts = pbmc.data, project = "pbmc3k",meta.data = pbmc.metadata)
+remove(pbmc.data,pbmc.metadata)
+
+pbmc <- NormalizeData(pbmc)
+pbmc <- FindVariableFeatures(pbmc, selection.method = "vst", nfeatures = 2000)
+all.genes <- rownames(pbmc)
+pbmc <- ScaleData(pbmc, features = all.genes)
+pbmc <- RunPCA(pbmc, features = VariableFeatures(object = pbmc))
+
+pbmc <- FindNeighbors(pbmc, dims = 1:20)
+pbmc <- FindClusters(pbmc, resolution = 0.8)
+pbmc <- RunUMAP(pbmc, dims = 1:20)
+pbmc <- RunTSNE(pbmc, dims = 1:20)
+remove(all.genes)
+Idents(pbmc)<-pbmc@meta.data$group
+
+
+hilomarkers<-FindMarkers(pbmc,only.pos = TRUE,ident.1 = "hi",ident.2 = "lo")
+write.table(hilomarkers,"c:/Users/xjmik/Desktop/GSE98638hilomarkers.txt",sep = "\t")
+remove(hilomarkers)
+name<-colnames(subset(pbmc,idents = "hi"))
+remove(pbmc)
+pbmc_raw$GROUP<-rep("other",length(colnames(pbmc_raw)))
+for (i in 1:length(name)) {
+  for (j in 1:length(colnames(pbmc_raw))) {
+    if(name[i] == colnames(pbmc_raw)[j]){
+      pbmc_raw@meta.data$GROUP[j] <- "hi"
+    }
+  }
+}
+
+remove(i,j)
+Idents(pbmc_raw)<-pbmc_raw$GROUP
+markers1<-FindMarkers(pbmc_raw,ident.1 = "hi",ident.2 = "other",only.pos = TRUE)
+write.table(markers1,file = "c:/Users/xjmik/Desktop/GSE98638hivsother.txt",sep = "\t")
+remove(name)
+remove(markers1)
+a<-read.table("c:/Users/xjmik/Desktop/1C_signature.txt",sep = "\t")
+a<-a[,1]
+pbmc_raw<-AddModuleScore(pbmc_raw,features = a,name = "J1C",random.seed = 1)
+remove(a)
+JMJD1C<-select(pbmc_raw@meta.data,starts_with("J1C"))
+pbmc_raw$JMJD1C_signature<-apply(JMJD1C,MARGIN = 1,median)
+remove(JMJD1C)
+Idents(pbmc_raw)<-pbmc_raw$majorCluster
+FeaturePlot(pbmc_raw,features = "JMJD1C_signature" ,label = TRUE)
